efc4410780cb88fdd92b5d46f31c691d616e3853
V <7334750+vchong@users.noreply.github.com>
2026-03-16 19:44:11 +0900

    Add cleanup and restart steps for Slack modifications
    Added cleanup steps to remove temporary files and restart Slack after modifications.

--- slack-notifications/notifications-slack.sh
+++ slack-notifications/notifications-slack.sh
@@ -1,34 +1,39 @@
+# Quit Slack
 # Create the directory ~/tmp/slack-backup if it doesn't already exist
- sudo mkdir -p ~/tmp/slack-backup
- # Copy the app.asar file from Slack's resources to the backup directory
- sudo cp /usr/lib/slack/resources/app.asar ~/tmp/slack-backup/
- # Copy the app.asar.unpacked directory from Slack's resources to the backup directory
- sudo cp -r /usr/lib/slack/resources/app.asar.unpacked ~/tmp/slack-backup/
- # Install npm (Node.js package manager)
- sudo apt install npm
- # Globally install the asar package to work with asar archives
- sudo npm install -g asar
- # Create a temporary directory for extracting the asar archive
- sudo mkdir ~/tmp/slack
- # Extract the contents of the app.asar archive into the /tmp/slack directory
- sudo asar extract /usr/lib/slack/resources/app.asar ~/tmp/slack
- # Remove the original app.asar file from Slack's resources directory
- sudo rm /usr/lib/slack/resources/app.asar
- # Remove the app.asar.unpacked directory from Slack's resources directory
- sudo rm -rf /usr/lib/slack/resources/app.asar.unpacked
- # Turn off blue dot notifications
- ## Ubuntu 20.04 n 22.04
- sudo sed -i 's/:t>0/:t<0/g' ~/tmp/slack/dist/main.*.js
- ## Ubuntu 24.04
- sudo sed -i 's/:t>0/:t<0/g' ~/tmp/slack/dist/main.*.cjs
- # Turn off red dot notifications (only if you want)
- ## Ubuntu 20.04 n 22.04
- echo "mod f Ubuntu 20.04 n 22.04. Ignore errors if not these OS"
- sudo sed -i 's/return i>0/return i<0/g' ~/tmp/slack/dist/main.*.js
- sudo sed -i 's/return n>0/return n<0/g' ~/tmp/slack/dist/main.*.js
- ## Ubuntu 24.04
- echo "mod f Ubuntu 24.04. Ignore errors if not this OS"
- sudo sed -i 's/return i>0/return i<0/g' ~/tmp/slack/dist/main.*.cjs
- sudo sed -i 's/return n>0/return n<0/g' ~/tmp/slack/dist/main.*.cjs
- # Re-pack the modified Slack files back into an app.asar archive
- sudo asar pack ~/tmp/slack /usr/lib/slack/resources/app.asar
+sudo mkdir -p ~/tmp/slack-backup
+# Copy the app.asar file from Slack's resources to the backup directory
+sudo cp /usr/lib/slack/resources/app.asar ~/tmp/slack-backup/
+# Copy the app.asar.unpacked directory from Slack's resources to the backup directory
+sudo cp -r /usr/lib/slack/resources/app.asar.unpacked ~/tmp/slack-backup/
+# Install npm (Node.js package manager)
+sudo apt install npm
+# Globally install the asar package to work with asar archives
+sudo npm install -g asar
+# Create a temporary directory for extracting the asar archive
+sudo mkdir ~/tmp/slack
+# Extract the contents of the app.asar archive into the /tmp/slack directory
+sudo asar extract /usr/lib/slack/resources/app.asar ~/tmp/slack
+# Remove the original app.asar file from Slack's resources directory
+sudo rm /usr/lib/slack/resources/app.asar
+# Remove the app.asar.unpacked directory from Slack's resources directory
+sudo rm -rf /usr/lib/slack/resources/app.asar.unpacked
+# Turn off blue dot notifications
+## Ubuntu 20.04 n 22.04
+sudo sed -i 's/:t>0/:t<0/g' ~/tmp/slack/dist/main.*.js
+## Ubuntu 24.04
+sudo sed -i 's/:t>0/:t<0/g' ~/tmp/slack/dist/main.*.cjs
+# Turn off red dot notifications (only if you want)
+## Ubuntu 20.04 n 22.04
+echo "mod f Ubuntu 20.04 n 22.04. Ignore errors if not these OS"
+sudo sed -i 's/return i>0/return i<0/g' ~/tmp/slack/dist/main.*.js
+sudo sed -i 's/return n>0/return n<0/g' ~/tmp/slack/dist/main.*.js
+## Ubuntu 24.04
+echo "mod f Ubuntu 24.04. Ignore errors if not this OS"
+sudo sed -i 's/return i>0/return i<0/g' ~/tmp/slack/dist/main.*.cjs
+sudo sed -i 's/return n>0/return n<0/g' ~/tmp/slack/dist/main.*.cjs
+# Re-pack the modified Slack files back into an app.asar archive
+sudo asar pack ~/tmp/slack /usr/lib/slack/resources/app.asar
+# Cleanup
+sudo rm -rf ~/tmp/slack/*
+sudo rm -rf ~/tmp/slack-backup/*
+# Restart Slack
